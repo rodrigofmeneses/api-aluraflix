d28b6dd1921eb3d15e0151d64378748cf8e229ca
Rodrigo Meneses <rodrigofmeneses97@gmail.com>
2022-10-25 15:34:25 -0300

    recuperar informacao do video pelo id

--- app/controllers/videos.py
+++ app/controllers/videos.py
@@ -8,3 +8,10 @@ videos = Blueprint('videos', __name__, url_prefix='/videos')
 def index():
     videos = Videos.query.all()
     return jsonify([video.serializer() for video in videos]), 200
+
+@videos.get('/<int:id>')
+def video_id(id):
+    video = Videos.query.filter_by(id=id).first()
+    if not video:
+        return jsonify({"message": "video not found"}), 404
+    return jsonify(video.serializer()), 200
--- tests/test_videos.py
+++ tests/test_videos.py
@@ -8,3 +8,11 @@ def test_videos_get():
         response = client.get('/videos/')
         assert response.status_code == 200
         assert b'"titulo": "Video teste 1"' in response.data
+
+def test_videos_get_by_id():
+    app = create_app()
+    
+    with app.test_client() as client:
+        response = client.get('/videos/2')
+        assert response.status_code == 200
+        assert b'"titulo": "Video teste 2"' in response.data
