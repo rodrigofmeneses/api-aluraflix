8781b392e2125a10f272e2f393dceb730069f2aa
rodrigofmeneses <rodrigo.menesesufc@gmail.com>
2022-11-14 13:45:41 -0300

    :recycle: refactor: Mudança no make_user para aproveitar a sintaxe de dicionário python.

--- app/ext/schemas/users_schema.py
+++ app/ext/schemas/users_schema.py
@@ -10,11 +10,12 @@ class UserSchema(ma.SQLAlchemySchema):
         model = User
     id = ma.auto_field(dump_only=True)
     username = ma.auto_field(validate=[not_blank])
-    password = ma.auto_field('password_hash', validate=[not_blank])
+    password = ma.auto_field('password_hash', validate=[not_blank], load_only=True)
     
     
     @post_load
     def make_user(self, data, **kwargs):
+        data['password'] = data.pop('password_hash')
         return User(**data)
         
 
