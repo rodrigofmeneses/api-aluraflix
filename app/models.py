ca7e28b108c257f4d991146f168f1857f1cd2030
rodrigofmeneses <rodrigo.menesesufc@gmail.com>
2022-11-14 12:32:37 -0300

    fix: Nome de usuário deve ser único

--- app/models.py
+++ app/models.py
@@ -25,7 +25,7 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(50), nullable=False, unique=True)
     password_hash = db.Column(db.String(255), nullable=False)
         
     @property
