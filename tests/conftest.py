18d285f831202f4aa740471f40ea6639b4762c19
rodrigofmeneses <rodrigo.menesesufc@gmail.com>
2022-11-16 18:23:03 -0300

    :test_tube: test: Adicionado testes para registro e autenticacao de usuario

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,10 @@ from app.ext.database import db
 from pytest import fixture
 from random import randint
 
-from app.models import Video, Category
+from app.models import User, Video, Category
 
 
-@fixture(scope='module')
+@fixture(scope="module")
 def client():
     app = create_app(FORCE_ENV_FOR_DYNACONF="testing")
     context = app.test_request_context()
@@ -18,21 +18,13 @@ def client():
         db.drop_all()
         db.session.remove()
 
+
 @fixture
 def categories(client):
     categories = [
-        Category(
-            title='LIVRE',
-            color='white'
-        ),
-        Category(
-            title='category teste 1',
-            color='color teste 1'
-        ),
-        Category(
-            title='category teste 2',
-            color='color teste 2'
-        )
+        Category(title="LIVRE", color="white"),
+        Category(title="category teste 1", color="color teste 1"),
+        Category(title="category teste 2", color="color teste 2"),
     ]
     db.session.bulk_save_objects(categories)
     db.session.commit()
@@ -53,15 +45,16 @@ def categories(client):
 def bulk_videos(client, categories):
     videos = [
         Video(
-            title=f'Video teste {i}',
-            description='Meu primeiro video',
-            url='https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-            category_id=randint(1, 3)
-        ) for i in range(20)
+            title=f"Video teste {i}",
+            description="Meu primeiro video",
+            url="https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+            category_id=randint(1, 3),
+        )
+        for i in range(20)
     ]
     db.session.bulk_save_objects(videos)
     db.session.commit()
-    
+
     yield videos
 
     try:
@@ -73,35 +66,57 @@ def bulk_videos(client, categories):
     finally:
         db.session.close()
 
+
 @fixture
 def videos(client, categories):
     videos = [
         Video(
-            title='Video teste 1',
-            description='Meu primeiro video',
-            url='https://www.google.com/'
+            title="Video teste 1",
+            description="Meu primeiro video",
+            url="https://www.google.com/",
         ),
         Video(
-            title='Video teste 2',
-            description='Meu segundo video',
-            url='https://www.google.com/',
-            category_id=2
+            title="Video teste 2",
+            description="Meu segundo video",
+            url="https://www.google.com/",
+            category_id=2,
         ),
         Video(
-            title='Video teste 3',
-            description='Meu terceiro video',
-            url='https://www.google.com/',
-            category_id=2
+            title="Video teste 3",
+            description="Meu terceiro video",
+            url="https://www.google.com/",
+            category_id=2,
         ),
     ]
     db.session.bulk_save_objects(videos)
     db.session.commit()
-    
+
     yield videos
 
     try:
         for video in Video.query.all():
             db.session.delete(video)
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+
+
+@fixture
+def users(client):
+    users = [
+        User(username='rodrigo', password='1234'),
+        User(username='marta', password='4321')
+    ]
+    db.session.bulk_save_objects(users)
+    db.session.commit()
+
+    yield users
+
+    try:
+        for user in User.query.all():
+            db.session.delete(user)
         db.session.commit()
     except:
         db.session.rollback()
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,8 +1,21 @@
-def test_register_user(client):
-    data = {
-        'username': 'RFM',
-        'password': '123'
-    }
-    response = client.post('auth/register/', json=data)
+from pytest import mark
+
+def test_register_user(client, users):
+    data = {"username": "RFM", "password": "123"}
+    response = client.post("auth/register/", json=data)
     assert response.status_code == 201
-    assert b'RFM' in response.data
+    assert b"RFM" in response.data
+
+def test_register_existent_username(client, users):
+    data = {"username": "rodrigo", "password": "abcd"}
+    client.post("auth/register/", json=data)
+    response = client.post("auth/register/", json=data)
+    assert response.status_code == 400
+    assert b"Username must be unique" in response.data
+
+def test_authenticate_user(client, users):
+    data = {"username": "rodrigo", "password": "1234"}
+    response = client.post("auth/", json=data)
+    assert response.status_code == 200
+    assert b"user authenticate" in response.data
+    assert b"token" in response.data
