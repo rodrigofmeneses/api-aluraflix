a976750bc3f6e2efa3d7b91e6659de1b83c62f00
rodrigofmeneses <rodrigo.menesesufc@gmail.com>
2022-11-21 00:27:57 -0300

    :bug: fix (Variaveis de ambiente): Importação da URI do banco de dados via variáveis de ambiente.

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from dynaconf import FlaskDynaconf
 
@@ -5,5 +6,6 @@ from dynaconf import FlaskDynaconf
 def create_app(**config):
     app = Flask(__name__)
     FlaskDynaconf(app, **config)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
     app.config.load_extensions("EXTENSIONS")
     return app
